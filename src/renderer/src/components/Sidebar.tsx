2d1b8ada43ecf88f65013a5720a81972e98daf23
min <minhsiungw@gmail.com>
2026-05-05 12:51:58 +0800

    feat: add sidebar bg color

--- src/renderer/src/components/Sidebar.tsx
+++ src/renderer/src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function Sidebar(): JSX.Element {
   return (
-    <nav className="flex flex-col gap-1">
+    <nav className="flex flex-col h-full gap-1 bg-sidebar">
       <Link to="/">Home</Link>
       <Link to="/pdf-notes">Carrie + Notes</Link>
     </nav>
